8ca196b6aea20d2137d5a7f8a949a58c1682853d
Gareis Pablo <gareispablotecnica1@gmail.com>
2025-09-27 16:18:48 -0300

    Agregar archivos de proyecto.

---
+++ CapaLogica/ValidacionLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class ValidacionLogin
+    {
+        public static bool Login(string Usuario, string Contraseña)
+        {
+            return (Usuario == "Admin" || Usuario == "admin") && Contraseña == "1234" ? true : false;
+        }
+    }
+}
---
+++ TestUnitario/Test1.cs
@@ -0,0 +1,15 @@
+﻿using CapaLogica;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool test = CapaLogica.ValidacionLogin.Login("Admin","1234");
+            Assert.IsTrue(test);
+        }
+    }
+}
